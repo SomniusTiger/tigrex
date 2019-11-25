6d0ef96d41ded85201ce1e27377dcc20f42231e3
Eoin Thomas O'Hehir <mail@etohehir.com>
2019-11-24 22:17:11 -0800

    Adds more favicons, figured out using emotion for styling

---
+++ components/shared/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Sigil from '../svg/Sigil';
+
+const Header = () => (
+  <section className="rc-Header">
+    <Sigil />
+    <h2>Tigrex: Somnius Codex</h2>
+  </section>
+);
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import emotionReset from 'emotion-reset';
 import { Global, css } from '@emotion/core';
 
+import Header from '../components/shared/Header';
 import Metatags from '../components/meta/Metatags';
 
 const Home = () => (
   <div>
     <Metatags />
+    <Header />
 
     <section className="hero">
       Hello yes this is cat
