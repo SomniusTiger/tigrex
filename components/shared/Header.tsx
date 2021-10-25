1f42889d1973e713050de01d8b778368bf141f1a
Eoin Thomas O'Hehir <meow@somni.us>
2021-10-24 18:31:01 -0700

    WIP - updating packages

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import styled from '@emotion/styled';
-
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import { colors, sizes } from '../styles_shared/variables';
 
-const HomeLink = styled('a')`
+const styles = {
+  headerWrapper: css`
+    align-items: center;
+    background-color: ${colors.grey_dark};
+    display: flex;
+    height: ${sizes.unit * 3}px;
+    padding: ${sizes.unit}px;
+  `,
+  headerText: css`
+    font-weight: bold;
+    margin-left: ${sizes.unit}px;
+  `,
+  homeLink: css`
   color: ${colors.white};
   text-decoration: none;
-`;
-
-const HeaderWrapper = styled('section')`
-  align-items: center;
-  background-color: ${colors.grey_dark};
-  display: flex;
-  height: ${sizes.unit * 3}px;
-  padding: ${sizes.unit}px;
-`;
-
-const HeaderText = styled('h2')`
-  font-weight: bold;
-  margin-left: ${sizes.unit}px;
-`;
+`,
+};
 
 const Header = () => (
-  <HeaderWrapper className="rc-Header">
-    <HeaderText>
-      <HomeLink href="/">Somnius</HomeLink>
-    </HeaderText>
-  </HeaderWrapper>
+  <section className="rc-Header" css={styles.headerWrapper}>
+    <h2 css={styles.headerText}>
+      <a css={styles.homeLink} href="/">Somnius</a>
+    </h2>
+  </section>
 );
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { Global, css } from '@emotion/core';
+/** @jsx jsx */
+import { Global, jsx, css } from '@emotion/core';
 
 import emotionReset from 'emotion-reset';
 
@@ -9,21 +8,25 @@ import { sizes } from '../components/styles_shared/variables';
 import Header from '../components/shared/Header';
 import Metatags from '../components/meta/Metatags';
 
-import Sigil from '../components/svg/Sigil';
-
-const Introduction = styled('section')`
-  padding: ${sizes.unit * 4}px ${sizes.unit}px 0 ${sizes.unit}px;
-  text-align: center;
-`;
+const styles = {
+  introduction: css`
+    padding: ${sizes.unit * 4}px ${sizes.unit}px 0 ${sizes.unit}px;
+    text-align: center;
+  `,
+  aboutText: css`
+    margin: ${sizes.unit * 3}px auto 0;
+    max-width: 600px;
+    width: 100%;
+  `,
+};
 
 const Home = () => (
   <div>
     <Metatags />
     <Header />
-
-    <Introduction>
-      <Sigil/>
-    </Introduction>
+    <div css={css`
+      background-color: #333;
+    `} />
 
     <Global styles={css`
       ${emotionReset}
@@ -33,6 +36,13 @@ const Home = () => (
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
       }
     `} />
+
+    <section css={styles.introduction}>
+      <img src="/images/somnius.svg" width="100%" alt="A small white tiger in a fancy outfit" />
+      <p css={styles.aboutText}>
+        Welcome. This is the digital home of Somnius. This is my personal wiki, where I can compile notes about both things I have made and other things I find it useful to share.
+      </p>
+    </section>
   </div>
 );
 
