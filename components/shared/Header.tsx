19a05abb924c26a886a2ad2d48c47256efb3ecf4
Eoin Thomas O'Hehir <meow@somni.us>
2021-10-24 18:57:41 -0700

    Updates packages, no emotion support yet, css is gone for now

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,29 +1,7 @@
-/** @jsx jsx */
-import { jsx, css } from '@emotion/core';
-import { colors, sizes } from '../styles_shared/variables';
-
-const styles = {
-  headerWrapper: css`
-    align-items: center;
-    background-color: ${colors.grey_dark};
-    display: flex;
-    height: ${sizes.unit * 3}px;
-    padding: ${sizes.unit}px;
-  `,
-  headerText: css`
-    font-weight: bold;
-    margin-left: ${sizes.unit}px;
-  `,
-  homeLink: css`
-  color: ${colors.white};
-  text-decoration: none;
-`,
-};
-
 const Header = () => (
-  <section className="rc-Header" css={styles.headerWrapper}>
-    <h2 css={styles.headerText}>
-      <a css={styles.homeLink} href="/">Somnius</a>
+  <section className="rc-Header">
+    <h2>
+      <a href="/">Somnius</a>
     </h2>
   </section>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,23 @@
-/** @jsx jsx */
-import { Global, jsx, css } from '@emotion/core';
-
-import emotionReset from 'emotion-reset';
-
-import { sizes } from '../components/styles_shared/variables';
+import type { NextPage } from 'next'
 
 import Header from '../components/shared/Header';
 import Metatags from '../components/meta/Metatags';
 
-const styles = {
-  introduction: css`
-    padding: ${sizes.unit * 4}px ${sizes.unit}px 0 ${sizes.unit}px;
-    text-align: center;
-  `,
-  aboutText: css`
-    margin: ${sizes.unit * 3}px auto 0;
-    max-width: 600px;
-    width: 100%;
-  `,
-};
-
-const Home = () => (
-  <div>
+const Home: NextPage = () => {
+  return (
+    <div>
     <Metatags />
     <Header />
-    <div css={css`
-      background-color: #333;
-    `} />
+    <div />
 
-    <Global styles={css`
-      ${emotionReset}
-      body {
-        background-color: #000;
-        color: #aaa;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-      }
-    `} />
-
-    <section css={styles.introduction}>
+    <section>
       <img src="/images/somnius.svg" width="100%" alt="A small white tiger in a fancy outfit" />
-      <p css={styles.aboutText}>
+      <p>
         Welcome. This is the digital home of Somnius. This is my personal wiki, where I can compile notes about both things I have made and other things I find it useful to share.
       </p>
     </section>
   </div>
-);
+  )
+}
 
-export default Home;
+export default Home
